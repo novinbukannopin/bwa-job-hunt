74c51b2a9e2d11a870708b703d58592e68dce074
novinbukannopin <novinardiany@outlook.com>
2024-01-23 16:06:13 +0700

    :memo: fix<context> : fix auth credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -51,7 +51,7 @@ export const authOptions: NextAuthOptions = {
             return token
         },
         async session({session, token, user}) {
-            session.user.id = token.id
+            session.user.id = token.id as string
             return session
         }
     }
